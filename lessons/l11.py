3c7f9888a3eac59abff78195da71de18190ebd3e
Isaac Wu <isaacwu2000@gmail.com>
2025-04-29 21:10:30 +0000

    Gave project details  guidance for l11

---
+++ lessons/l11.py
@@ -0,0 +1,15 @@
+"""
+Make a calulator that takes in the user-inputted a and b as well as some operation (+, -, *, /) and outputs the result
+If you finish, you can add a 'sqrt' function and a 'square' function
+
+Hint:
+You can use functions for each operation (eg. add, subtract, multiply, divide) 
+and if statements to determine which operation the user selects
+"""
+
+a = input("n0: ")
+b = input("n1: ")
+def sum(x, y):
+    return x+y 
+
+print(sum(a, b))
