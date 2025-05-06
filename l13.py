af024ce1a50d8d856c408ff1ff09acca0701a32d
Isaac Wu <isaacwu2000@gmail.com>
2025-05-06 21:27:47 +0000

    Showed input func

--- l13.py
+++ l13.py
@@ -1,3 +1,5 @@
+
+"""
 user_correct_answers = 0
 while user_correct_answers < 3:
     user_answer = int(input("1 + 1 = "))
@@ -6,3 +8,7 @@ while user_correct_answers < 3:
         print("correct")
     else:
         print("incorrect")
+"""
+
+x = input("What is the square root of 36? ")
+print("The use inputted the answer of " + x)
