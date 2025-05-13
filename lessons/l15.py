b81bad06b276cf34c9d6bad00423a26f0fc56e94
Isaac Wu <isaacwu2000@gmail.com>
2025-05-13 21:10:32 +0000

    posted other assignments

--- lessons/l15.py
+++ lessons/l15.py
@@ -2,4 +2,20 @@ for i in range(1, 10):
     print(i)
 
 """Make a program that solves a diophantine equation ax+by=c for one integer solution. 
-Test the numbers -1000 to 1000 for both x and y"""
+Test the numbers -1000 to 1000 for both x and y.
+Note that there may not be a solution."""
+
+"""
+Fizzbuzz
+ 
+For every number from 1 to 100 inclusive,
+- if that number is divisble by 2, print "fizz"
+- if that number is divisble by 3, print "buzz"
+- if that number is divisble by both 2 and 3, print "fizzbuzz"
+"""
+
+"""Mini Project: Trivia Game (remainder of class) 
+    1.  Create at least 3 trivia questions, and prompt the user. 
+    2.  Count how many the user gets right, and if they get a score above 70%, they win! 
+    3.  If the user fails, they have to redo the trivia quiz until they win. 
+    4.  Make sure to randomize the order the questions go in."""
