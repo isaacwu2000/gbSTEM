f73665a321f9f4b45f04b1b5b15dad413b4fdf68
Isaac Wu <isaacwu2000@gmail.com>
2025-05-14 00:37:03 +0000

    edits

--- lessons/l15.py
+++ lessons/l15.py
@@ -1,6 +1,3 @@
-for i in range(1, 10):
-    print(i)
-
 """Make a program that solves a diophantine equation ax+by=c for one integer solution. 
 Test the numbers -1000 to 1000 for both x and y.
 Note that there may not be a solution."""
@@ -19,3 +16,16 @@ For every number from 1 to 100 inclusive,
     2.  Count how many the user gets right, and if they get a score above 70%, they win! 
     3.  If the user fails, they have to redo the trivia quiz until they win. 
     4.  Make sure to randomize the order the questions go in."""
+
+"""
+# nested for loop
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(i*j)
+        """
+
+# find a solutions to 2x+y=300
+for y in range(1, 101):
+    for x in range(1, 101):
+        if 2*x+y==300:
+            print(x, y)
