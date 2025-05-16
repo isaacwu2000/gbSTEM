269b6ee51ae061429be3a584a39c676eb422938d
Isaac Wu <isaacwu2000@gmail.com>
2025-05-16 12:59:46 +0000

    finihed lesson 16

--- lessons/l16.py
+++ lessons/l16.py
@@ -1,8 +1,7 @@
 """
 Print out every item in a list in with a for i in range loop
 """
+nums = [0, 1, 2]
+for num in nums:
+    print(num)
 
-instruments = ["piano", "trumpet", "flute", "drums"]
-instruments.insert(0, "trumbone")
-
-print(instruments)
