ff8b8ac10e0c4624fd7e32bd35e45cc8324d43a4
Isaac Wu <isaacwu2000@gmail.com>
2025-05-20 22:52:10 +0000

    finsihed 117

--- lessons/l16.py
+++ lessons/l16.py
@@ -4,4 +4,3 @@ Print out every item in a list in with a for i in range loop
 nums = [0, 1, 2]
 for num in nums:
     print(num)
-
---
+++ lessons/l17.py
@@ -0,0 +1,24 @@
+
+"""
+Print out every item in a list in with a for i in range loop
+Find the minimum of a list
+Sort all the numbers in a list
+"""
+
+"""
+Manual solution to 1
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+"""
+
+fruits = ['banana', 'apple', 'strawberry', 'peach']
+print(len(fruits))
+#print(fruits.index('banana'))
+#print(fruits[0])
+#fruits.append('grape')
+#fruits.remove('strawberry')
+"""
+for fruit in fruits:
+    print(fruit)
+"""
