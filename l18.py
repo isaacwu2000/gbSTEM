b65242451507d3f6a31324de0c71970c8ddb52c8
Isaac Wu <isaacwu2000@gmail.com>
2025-05-22 20:57:48 +0000

    added exercise to l18

--- l18.py
+++ l18.py
@@ -9,3 +9,11 @@ grid = [
 for row in grid:
     for col in row:
         print(col)
+
+"""
+Given a 2d Array, one row numbers and one row with letters, 
+make a new array that combines the two lists into
+[a1, b2, c3, d4]
+and prints the result
+]
+"""
