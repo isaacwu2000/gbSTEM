2bb4fc5f91454fd003b28a9f8afc8da914a89ece
Isaac Wu <isaacwu2000@gmail.com>
2025-06-03 21:17:42 +0000

    explained loops and 2d arrays again

---
+++ lessons/l21.py
@@ -0,0 +1,25 @@
+"""
+count = 0
+while True:
+    count += 1
+    print(count)
+    if count==10:
+        break
+print("Loop broken") 
+
+game_board = [
+    ['x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x']
+]
+
+[
+    ['x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'o'],
+    ['x', 'x', 'o', 'o'],
+    ['g', 'o', 'g', 'g']
+]
+
+
+"""
